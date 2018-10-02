5fb6a76443eee4e4addc14f369eb87bd21984546
subaru-shoji <32668181+subaru-shoji@users.noreply.github.com>
2018-10-02 18:51:46 +0900

    Add WebM Representation. (#1)

--- lib/bitmovin/encoding/manifests/video_adaptation_set.rb
+++ lib/bitmovin/encoding/manifests/video_adaptation_set.rb
@@ -1,6 +1,7 @@
 module Bitmovin::Encoding::Manifests
   class VideoAdaptationSet < Bitmovin::Resource
     include Bitmovin::ChildCollection
+
     def initialize(manifest_id, period_id, hash = {})
       path = File.join("/v1/encoding/manifests/dash/", manifest_id, "periods", period_id, "adaptationsets/video")
       self.class.init(path)
@@ -10,6 +11,7 @@ module Bitmovin::Encoding::Manifests
     end
 
     child_collection(:fmp4_representations, "/v1/encoding/manifests/dash/%s/periods/%s/adaptationsets/%s/representations/fmp4", [:manifest_id, :period_id, :id], Fmp4Representation)
+    child_collection(:webm_representations, "/v1/encoding/manifests/dash/%s/periods/%s/adaptationsets/%s/representations/webm", [:manifest_id, :period_id, :id], WebmRepresentation)
 
     attr_accessor :manifest_id
     attr_accessor :period_id
